d1e76ba5ad47d32bf96400a31b72a61771b95e0b
Braeden Robak <braeden@braeden.io>
2018-06-18 16:43:01 -0600

    Add GameRules subdirectory, GameManager and GameManagerTests
    Added GameRules subdirectory to store all use cases controllers for the entities, ie the rules of the game that controller the entities.
    Added GameManger which instantiates two players and controlls the flow of actions performed on them.
    Added GameManager tests to test said flow of controll.

---
+++ Assets/Code/Editor/Tests/EditModeTests/GameManagerTests.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class GameManagerTests {
+
+    private GameManager _gameManager;
+
+    [SetUp]
+    public void Setup()
+    {
+        _gameManager = new GameManager();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        _gameManager = null;
+    }
+
+    [Test]
+    public void GivesEnergyToPlayers()
+    {
+        _gameManager.AddEnergyToPlayers();
+
+        Assert.AreEqual(4, _gameManager.GetPlayersCurrentEnergy(Players.PlayerOne));
+        Assert.AreEqual(4, _gameManager.GetPlayersCurrentEnergy(Players.PlayerTwo));
+    }
+
+    [Test]
+    public void FalseOnPlayerNotHavingEnoughEnergy()
+    {
+        Assert.False(_gameManager.PlayerCanCastEnergyOfCost(Players.PlayerOne, 5));
+    }
+
+    [Test]
+    public void TrueOnPlayerHasEnoughEnergy()
+    {
+        Assert.True(_gameManager.PlayerCanCastEnergyOfCost(Players.PlayerOne, 3));
+    }
+
+    [Test]
+    public void DamagesPlayer()
+    {
+        _gameManager.DamagePlayer(Players.PlayerTwo, 2);
+
+        Assert.AreEqual(1, _gameManager.GetPlayersCurrentHealth(Players.PlayerTwo));
+    }
+
+
+
+}
---
+++ Assets/Code/GameRules/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Players
+{
+    PlayerOne = 0,
+    PlayerTwo = 1
+};
+
+
+public class GameManager {
+
+    private PlayerModel[] _players;
+
+    public GameManager()
+    {
+        _players = new PlayerModel[2];
+
+        _players[(int)Players.PlayerOne] = new PlayerModel(3, 3);
+        _players[(int)Players.PlayerTwo] = new PlayerModel(3, 3);
+    }
+
+    public int GetPlayersCurrentEnergy(Players id)
+    {
+        return _players[(int)id].GetEnergy();
+    }
+
+    public int GetPlayersCurrentHealth(Players id)
+    {
+        return _players[(int)id].GetHealth();
+    }
+
+    public void AddEnergyToPlayers()
+    {
+        foreach(PlayerModel player in _players)
+        {
+            player.GainEnergy(1);
+        }
+    }
+
+    public bool PlayerCanCastEnergyOfCost(Players id, int energyCost)
+    {
+        return _players[(int)id].HasEnoughEnergy(energyCost);
+    }
+	
+    public void DamagePlayer(Players id, int damage)
+    {
+        _players[(int)id].Damage(damage);
+    }
+}
