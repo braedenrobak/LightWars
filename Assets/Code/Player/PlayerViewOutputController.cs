2949b19d895be96e0b8b014476c633613249b5dd
Braeden Robak <braeden@braeden.io>
2018-06-21 02:00:55 -0600

    Created the initial setup for PlayerVIewOutputController
    Created the PlayerViewOutputController which implements the IGameViewOutputController. Initially stores the PlayerView in the scene and transmits the output form GameController to the appropriate PlayerView.

---
+++ Assets/Code/Player/PlayerViewOutputController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerViewOutputController : MonoBehaviour, IGameViewOutputController
+{
+    private PlayerView[] _playerViews;
+
+    public PlayerViewOutputController(PlayerView[] playerViews)
+    {
+        _playerViews = playerViews;
+    }
+
+    public void DisplayPlayerHit(PlayerIds playerId, int damage)
+    {
+        _playerViews[(int)playerId].DisplayHit(damage);
+    }
+
+    public void GameOverWithWinner(PlayerIds playerId)
+    {
+       // Open up winner winner light war dinner screen and display playerId as victorious
+    }
+
+    public void UpdatePlayerView(PlayerData playerData)
+    {
+        _playerViews[(int)playerData.id].UpdateVisual(playerData);
+    }
+}
