ad87695e2297fb2bd1d72ba21a3cd367c0915d2c
Braeden Robak <braeden@braeden.io>
2018-06-25 16:43:27 -0600

    Inital boilerplate code created
    Initial created the implementation of IPlayerOutputController with NetworkPlayerOutput and NetworkPlayerView in accordance to the current design of the game.

---
+++ Assets/Code/Networking/NetworkPlayerOutputController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPlayerOutputController : NetworkBehaviour, IPlayerViewOutputController
+{
+    // ** TODO : DECIDE IF IT NEEDS TO BE DECIED IF ITS SERVER OR NOT BEFORE UPDATING
+    public void DisplayPlayerHit(PlayerIds playerId, int damage)
+    {
+       // Play damage animation at the playerId's position
+    }
+
+    public void GameOverWithWinner(PlayerIds playerId)
+    {
+        // Load winner stuff
+    }
+
+    public void UpdatePlayerView(PlayerData playerData)
+    {
+        // Send update to the PlayerView
+    }
+}
---
+++ Assets/Code/Networking/NetworkPlayerView.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkPlayerView : MonoBehaviour {
+
+	// All about updating visual
+    // This is where you connect the variables to be chagned across clients when changed
+    // If server, it updates and sends to all clients
+}
