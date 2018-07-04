3caa3e17e58f6374baeacd4182dc54c7c21ed2ce
Braeden Robak <braeden@braeden.io>
2018-07-03 19:16:59 -0600

    Added BaseEnergySpawner for different game mode spawners to inherit from
    BaseEnergySpawner setups up connection and communication with EnergyManager to provide like functionality between any game modes energy spawning.

---
+++ Assets/Code/Energies/BaseEnergySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseEnergySpawner {
+
+    // Give something for player input to know it is able to call to spawn an energy with certain type and position
+    protected EnergyManager _energyManager;
+
+    public abstract void SpawnEnergyOfType(int energyType, Vector3 position);
+
+    public void SetEnergyManager(EnergyManager energyManager)
+    {
+        _energyManager = energyManager;
+    }
+
+    public List<EnergyType> GetEnergiesThatCanSpawn()
+    {
+        return _energyManager.GetEnergyTypes();
+    }
+
+    private EnergyObjectData GetEnergyObjectDataOfType(int energyType)
+    {
+        return _energyManager.GetEnergyOfType(energyType);
+    }
+}
