c7f495972120090e5205850799a2c8db77290825
Braeden Robak <braeden@braeden.io>
2018-07-09 13:30:32 -0600

    Cleaned up NetworkEnergyView Movement code
    Deleted the unneccesary percentage and start/end position variables and calculations.

--- Assets/Code/Multiplayer/Energy/NetworkEnergyView.cs
+++ Assets/Code/Multiplayer/Energy/NetworkEnergyView.cs
@@ -19,12 +19,7 @@ public class NetworkEnergyView : NetworkBehaviour {
         _speed = speed;
     }
 
-    private Vector3 _startPosition;
-    private Vector3 _endPosition;
-
     private float directionInverter = 1.0f;
-
-    private float _percentFinished = 0.0f;
 
     public void SetOwner(int playerId)
     {
@@ -38,17 +33,15 @@ public class NetworkEnergyView : NetworkBehaviour {
 
     public void Start()
     {
-        _startPosition = GameObject.Find("Player " + _ownerId).transform.position;
-        transform.position = _startPosition;
+        Vector3 startPosition = GameObject.Find("Player " + _ownerId).transform.position;
+        transform.position = startPosition;
 
-        if (_startPosition == Constants.LOCAL_PLAYER_POSITION)
+        if (startPosition == Constants.LOCAL_PLAYER_POSITION)
         {
-            _endPosition = Constants.NON_LOCAL_PLAYER_POSITION;
             directionInverter = 1.0f;
         }
         else
         {
-            _endPosition = Constants.LOCAL_PLAYER_POSITION;
             directionInverter = -1.0f;
         }
     }
@@ -56,19 +49,7 @@ public class NetworkEnergyView : NetworkBehaviour {
 
     public void Update()
     {
-        Vector3 currentDesiredPosition = transform.position + new Vector3(0, (_speed * Time.deltaTime * directionInverter));
-
-        _percentFinished = PercentageInbetween(_startPosition, _endPosition, currentDesiredPosition);
-
-        transform.position = Vector3.Lerp(_startPosition, _endPosition, _percentFinished);
-    }
-
-    private float PercentageInbetween(Vector3 start, Vector3 end, Vector3 current)
-    {
-        float totalDistance = Vector3.Distance(start, end);
-        float currentDistance = Vector3.Distance(current, end);
-
-        return 1.0f - (currentDistance / totalDistance);
+        transform.position += new Vector3(0, (_speed * Time.deltaTime * directionInverter));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
