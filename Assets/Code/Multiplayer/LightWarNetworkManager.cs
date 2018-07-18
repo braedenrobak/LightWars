9cdb9c6ece9c427d74c5e44f36fe882448d38779
Braeden Robak <braeden@braeden.io>
2018-07-18 13:13:14 -0600

    Commit clean up

--- Assets/Code/Multiplayer/LightWarNetworkManager.cs
+++ Assets/Code/Multiplayer/LightWarNetworkManager.cs
@@ -7,12 +7,10 @@ public class LightWarNetworkManager : NetworkManager {
 
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
     {
-
         GameObject player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
 
         GameObject.Find("NetworkMain").GetComponent<NetworkMain>().AddPlayer(player);
 
         NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
-
     }
 }
