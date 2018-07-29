c757e8ffcb537f21c5ff9a24329adb0d9a1322ef
Braeden Robak <braeden@braeden.io>
2018-07-29 13:08:18 -0600

    Added camera adjuster to scale FOV to current aspect ratio
    Adjusts camera FOV based on the desired aspect ratio and the current aspect ratio.

---
+++ Assets/CameraAspectAdjuster.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAspectAdjuster : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float targetAspect = 3.0f / 2.0f;
+        float windowAspect = (float)Screen.width / (float)Screen.height;
+
+        float scaleHeight = windowAspect / targetAspect;
+
+        Camera camera = GetComponent<Camera>();
+
+        if(scaleHeight < 1.0f)
+        {
+            Rect rect = camera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+
+            camera.rect = rect;
+        }
+        else
+        {
+            float scaleWidth = 1.0f / scaleHeight;
+
+            Rect rect = camera.rect;
+
+            rect.width = scaleWidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+	}
+}
