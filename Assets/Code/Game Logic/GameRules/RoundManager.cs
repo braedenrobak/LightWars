4c7fb8bfbc8aaf973e81c06c9d813cf3f81f9e1d
Braeden Robak <braeden@braeden.io>
2018-08-01 15:01:35 -0600

    Added RoundManager to GameLogic
    Created associated unit tests for RoundManager.
    RoundManager stores the number of rounds for a match and calculates the number of rounds needed to be won in order to win a match. It also stores the id of a winner with the associated round they have won and calculates when a unique player id has won enough rounds to win the match.

---
+++ Assets/Code/Game Logic/GameRules/RoundManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundManager {
+
+    private int _numberOfRounds;
+    private int _currentRound;
+
+    private List<int> _roundWinners;
+
+    public RoundManager(int numberOfRounds)
+    {
+        _numberOfRounds = numberOfRounds;
+        _currentRound = 1;
+
+        _roundWinners = new List<int>();
+    }
+
+    public int NumberOfRounds()
+    {
+        return _numberOfRounds;
+    }
+
+    public int GetBestOfRoundCount()
+    {
+        return (_numberOfRounds / 2) + 1;
+    }
+
+    public int GetCurrentRound()
+    {
+        return _currentRound;
+    }
+
+    public void EndRound(int winnerOfRoundID)
+    {
+        _roundWinners.Add(winnerOfRoundID);
+        _currentRound++;
+    }
+
+    public int HasWinner()
+    {
+        List<int> winners = _roundWinners;
+
+        winners.Sort();
+
+        int roundCount = 0;
+        int lastWinner = -1;
+        foreach(int winnerId in winners)
+        {
+            if(winnerId == lastWinner)
+            {
+                roundCount++;
+            }
+            else
+            {
+                roundCount = 1;
+            }
+
+            lastWinner = winnerId;
+
+            if (roundCount == GetBestOfRoundCount())
+                return winnerId;
+        }
+
+        return -1;
+    }
+
+    public List<int> GetRoundWinners()
+    {
+        return _roundWinners;
+    }
+
+}
---
+++ Assets/Code/Tests/Editor/RoundManagerTests.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class RoundManagerTests{
+
+    private RoundManager _roundManager;
+
+    [SetUp]
+    public void Setup()
+    {
+        _roundManager = new RoundManager(5);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _roundManager = null;
+    }
+
+    [Test]
+    public void StoreCorrectNumberOfRounds()
+    {
+        Assert.AreEqual(5, _roundManager.NumberOfRounds());
+    }
+
+    [Test]
+    public void CreatesBestOfBasedOnNumberOfRounds()
+    {
+        Assert.AreEqual(3, _roundManager.GetBestOfRoundCount());
+
+        _roundManager = new RoundManager(6);
+
+        Assert.AreEqual(4, _roundManager.GetBestOfRoundCount());
+    }
+
+    [Test]
+    public void TracksCurrentRound()
+    {
+        Assert.AreEqual(1, _roundManager.GetCurrentRound());
+
+        _roundManager.EndRound(42);
+
+        Assert.AreEqual(2, _roundManager.GetCurrentRound());
+    }
+
+    [Test]
+    public void StoresWinnerWithRoundTheyHaveWon()
+    {
+        int winnerId = 42;
+
+        _roundManager.EndRound(winnerId);
+
+        Assert.AreEqual(42, _roundManager.GetRoundWinners()[0]);
+    }
+
+    [Test]
+    public void ReturnsWinnerIfBestOfIsReached()
+    {
+        _roundManager.EndRound(1);
+
+        Assert.IsTrue(_roundManager.HasWinner() == -1);
+
+        _roundManager.EndRound(1);
+
+        Assert.IsTrue(_roundManager.HasWinner() == -1);
+
+        _roundManager.EndRound(1);
+
+        Assert.IsTrue(_roundManager.HasWinner() == 1);
+
+
+
+
+        _roundManager = new RoundManager(3);
+
+        _roundManager.EndRound(0);
+
+        Assert.IsTrue(_roundManager.HasWinner() == -1);
+
+        _roundManager.EndRound(1);
+
+        Assert.IsTrue(_roundManager.HasWinner() == -1);
+
+        _roundManager.EndRound(0);
+
+        Assert.IsTrue(_roundManager.HasWinner() == 0);
+    }
+
+    [Test]
+    public void SuddenDeathOnTieGameWithEvenNumberOfRounds()
+    {
+        _roundManager = new RoundManager(2);
+
+        _roundManager.EndRound(0);
+
+        _roundManager.EndRound(1);
+
+        Assert.IsTrue(_roundManager.HasWinner() == -1);
+
+        _roundManager.EndRound(0);
+
+        Assert.IsTrue(_roundManager.HasWinner() == 0);
+    }
+}
